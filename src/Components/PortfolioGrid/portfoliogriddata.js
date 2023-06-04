44db41cc5dfc0e4419a14ed13698a6568a2e0be8
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2023-06-05 01:11:56 +0530

    kgr website added

--- src/Components/PortfolioGrid/portfoliogriddata.js
+++ src/Components/PortfolioGrid/portfoliogriddata.js
@@ -8,64 +8,74 @@ export const imgcol1=[
     },
     {
         id:2,
+        title:'//travel_website_template',
+        href:'https://travel-site-616e0.firebaseapp.com/',
+        url:'/images/portfolioimg/travelsite.jpeg',
+        desc:'React.js | SASS | Firebase'
+    },
+    {
+        id:6,
         title:'//weather_app',
         href:'https://kavya-25.github.io/Weather-App/',
         url:'/images/portfolioimg/weather.png',
         desc:'HTML | CSS '
     },
-    {
-        id:3,
-        title:'//magic_notes',
-        href:'https://hillffairnith.com/',
-        url:'/images/portfolioimg/magicnotes.png',
-        desc:'HTML | CSS'
-    }
+    
 ]
 export const imgcol2=[
     {
-        id:4,
-        title:'//travel_site_template',
-        href:'https://travel-site-616e0.firebaseapp.com/',
-        url:'/images/portfolioimg/travelsite.jpeg',
-        desc:'React.js | SASS | Firebase'
+        id:3,
+        title:'//kgrgroup_website',
+        href:'https://kgrgroup.vercel.app/',
+        url:'/images/portfolioimg/kgrgroup.jpeg',
+        desc:'React.js | SASS '
     },
    
     {
-        id:5,
+        id:4,
         title:'//nimbus_2022',
         href:'https://festnimbus.com/',
         url:'/images/portfolioimg/nimbus.png',
         desc:'React.js | SASS | Node.js '
     },
     {
-        id:9,
+        id:5,
         title:'//to_do app',
         href:'https://kavya-25.github.io/PixoWebProjects/to-do-list/',
         url:'/images/portfolioimg/todo.png',
         desc:'HTML | CSS'
     },
    
+   
 ]
 export const imgcol3=[
     {
-        id:6,
+        id:8,
         title:'//my_portfolio',
         href:'https://kavya-25.github.io/PixoWebProjects/musicplayer/',
         url:'/images/portfolioimg/portfolio.png',
         desc:'React.js | SASS | Firebase'
     },
     {
-        id:7,
+        id:9,
         title:'//music_player',
         href:'https://kavya-25.github.io/PixoWebProjects/musicplayer/',
         url:'/images/portfolioimg/music.png',
         desc:'HTML | CSS'
     },
     {
-        id:8,
+        id:10,
         title:'//insta_clone',
         href:'https://insta-clone-1ff26.web.app/',
         url:'/images/portfolioimg/instaclone.jpeg',
         desc:'React.js | SASS | Firebase'
     },
+    
+    {
+        id:7,
+        title:'//magic_notes',
+        href:'https://hillffairnith.com/',
+        url:'/images/portfolioimg/magicnotes.png',
+        desc:'HTML | CSS'
+    }
 ]

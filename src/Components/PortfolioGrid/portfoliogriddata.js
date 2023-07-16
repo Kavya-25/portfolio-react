fe4808d70f56d6ebf43dd74ecd8226fc8d17858c
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2023-07-17 01:52:21 +0530

    resume builder added

--- src/Components/PortfolioGrid/portfoliogriddata.js
+++ src/Components/PortfolioGrid/portfoliogriddata.js
@@ -1,81 +1,85 @@
-export const imgcol1=[
-    {
-        id:1,
-        title:'//hillffair_nith',
-        href:'https://hillffairnith.com/',
-        url:'/images/portfolioimg/hillffair.jpeg',
-        desc:'React.js | SASS | Firebase'
-    },
-    {
-        id:2,
-        title:'//travel_website_template',
-        href:'https://travel-site-616e0.firebaseapp.com/',
-        url:'/images/portfolioimg/travelsite.jpeg',
-        desc:'React.js | SASS | Firebase'
-    },
-    {
-        id:6,
-        title:'//weather_app',
-        href:'https://kavya-25.github.io/Weather-App/',
-        url:'/images/portfolioimg/weather.png',
-        desc:'HTML | CSS | Javascript'
-    },
-    
-]
-export const imgcol2=[
-    {
-        id:3,
-        title:'//kgrgroup_website',
-        href:'https://kgrgroup.vercel.app/',
-        url:'/images/portfolioimg/kgrgroup.jpeg',
-        desc:'React.js | SASS '
-    },
-   
-    {
-        id:4,
-        title:'//nimbus_2022',
-        href:'https://festnimbus.com/',
-        url:'/images/portfolioimg/nimbus.png',
-        desc:'React.js | SASS | Node.js '
-    },
-    {
-        id:5,
-        title:'//to_do app',
-        href:'https://kavya-25.github.io/PixoWebProjects/to-do-list/',
-        url:'/images/portfolioimg/todo.png',
-        desc:'HTML | CSS | Javascript'
-    },
-   
-   
-]
-export const imgcol3=[
-    {
-        id:8,
-        title:'//my_portfolio',
-        href:'https://kavya-25.github.io/PixoWebProjects/musicplayer/',
-        url:'/images/portfolioimg/portfolio.png',
-        desc:'React.js | SASS | Firebase'
-    },
-    {
-        id:9,
-        title:'//music_player',
-        href:'https://kavya-25.github.io/PixoWebProjects/musicplayer/',
-        url:'/images/portfolioimg/music.png',
-        desc:'HTML | CSS | Javascript'
-    },
-    {
-        id:10,
-        title:'//instagram_clone',
-        href:'https://insta-clone-1ff26.web.app/',
-        url:'/images/portfolioimg/instaclone.jpeg',
-        desc:'React.js | SASS | Firebase'
-    },
-    
-    {
-        id:7,
-        title:'//magic_notes',
-        href:'https://hillffairnith.com/',
-        url:'/images/portfolioimg/magicnotes.png',
-        desc:'HTML | CSS | Javascript'
-    }
-]
+export const imgcol1 = [
+  {
+    id: 11,
+    title: "//resume_builder",
+    href: "https://resume-builder-virid.vercel.app/",
+    url: "/images/portfolioimg/resumebuilder.jpeg",
+    desc: "React.js | SASS ",
+  },
+  {
+    id: 1,
+    title: "//hillffair_nith",
+    href: "https://hillffairnith.com/",
+    url: "/images/portfolioimg/hillffair.jpeg",
+    desc: "React.js | SASS | Firebase",
+  },
+  {
+    id: 2,
+    title: "//travel_website_template",
+    href: "https://travel-site-616e0.firebaseapp.com/",
+    url: "/images/portfolioimg/travelsite.jpeg",
+    desc: "React.js | SASS | Firebase",
+  },
+];
+export const imgcol2 = [
+  {
+    id: 3,
+    title: "//kgrgroup_website",
+    href: "https://kgrgroup.vercel.app/",
+    url: "/images/portfolioimg/kgrgroup.jpeg",
+    desc: "React.js | SASS ",
+  },
+
+  {
+    id: 4,
+    title: "//nimbus_2022",
+    href: "https://festnimbus.com/",
+    url: "/images/portfolioimg/nimbus.png",
+    desc: "React.js | SASS | Node.js ",
+  },
+  {
+    id: 6,
+    title: "//weather_app",
+    href: "https://kavya-25.github.io/Weather-App/",
+    url: "/images/portfolioimg/weather.png",
+    desc: "HTML | CSS | Javascript",
+  },
+  {
+    id: 9,
+    title: "//music_player",
+    href: "https://kavya-25.github.io/PixoWebProjects/musicplayer/",
+    url: "/images/portfolioimg/music.png",
+    desc: "HTML | CSS | Javascript",
+  },
+];
+export const imgcol3 = [
+  {
+    id: 8,
+    title: "//my_portfolio",
+    href: "https://kavya-25.github.io/PixoWebProjects/musicplayer/",
+    url: "/images/portfolioimg/portfolio.png",
+    desc: "React.js | SASS | Firebase",
+  },
+  {
+    id: 10,
+    title: "//instagram_clone",
+    href: "https://insta-clone-1ff26.web.app/",
+    url: "/images/portfolioimg/instaclone.jpeg",
+    desc: "React.js | SASS | Firebase",
+  },
+
+  {
+    id: 7,
+    title: "//magic_notes",
+    href: "https://hillffairnith.com/",
+    url: "/images/portfolioimg/magicnotes.png",
+    desc: "HTML | CSS | Javascript",
+  },
+  {
+    id: 5,
+    title: "//to_do app",
+    href: "https://kavya-25.github.io/PixoWebProjects/to-do-list/",
+    url: "/images/portfolioimg/todo.png",
+    desc: "HTML | CSS | Javascript",
+  },
+];
